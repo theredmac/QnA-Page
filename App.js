6df58df92c4149c1bdc2fb14185f55f22a5d0785
Shubham Prakash <98254857+ShubhamP-inst@users.noreply.github.com>
2022-06-02 23:15:44 +0530

    Landing page added

---
+++ App.js
@@ -0,0 +1,17 @@
+import Landing_page from "./Landing_page";
+// import Animation from "./Animation.js";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <div className="animate">
+      <animation/>
+      </div> */}
+      <div className="content">
+        <Landing_page />
+      </div>
+    </div>
+  );
+}
+
+export default App;
