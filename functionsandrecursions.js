b171ffe6ccc322fecfcbe8ddf4169cc3585e5cb0
Krishna <105001537+Krishna-D-K@users.noreply.github.com>
2022-06-02 22:38:57 +0530

    Add files via upload

---
+++ functionsandrecursions.js
@@ -0,0 +1,29 @@
+import data from "./data.json";
+import "./styles.css";
+
+const FunctionsRecursions = () => {
+    let num=1;
+    return (
+        <>
+        <div className="title"> Lab- Problems: Functions and Recursions</div>
+        {data.functionsAndRecursions[0].Elements.map((data) =>(
+                <div className="datapreview" key={num}>
+                    <details>
+                    <summary id="ques"><div  id="question"># Question- {num++}</div></summary>
+                    <h3 ><pre>{data.Question}</pre></h3>
+                    <details>
+                    <summary id="code">Code:
+                    </summary>
+                    
+                    <div className="answer"><pre className="line-numbers"><code className="language-cpp">{data.Answer}</code></pre></div>
+                    
+                    </details>
+                    </details>
+                    {/* <button onClick={() => func(data.id)}> Delete</button> */}
+                </div>
+                ))}
+                </>
+    );
+}
+ 
+export default FunctionsRecursions;
